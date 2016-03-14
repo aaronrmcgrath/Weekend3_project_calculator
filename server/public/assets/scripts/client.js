c5e10fc11ac2bfbda0b5b8e1f6a1b0cbbd546301
Mbob023 <armcgrathmedia@gmail.com>
2016-03-14 00:15:42 -0500

    working

--- server/public/assets/scripts/client.js
+++ server/public/assets/scripts/client.js
@@ -1,140 +1,58 @@
-// var equation = {
-//   numX: 0,
-//   numY: 0,
-//   operand: ''
-// };
-
-// var numX = null;
-// var numY = null;
-// var operand = '';
 
 var values = {};
 
 $(document).ready(function(){
   console.log('This works!');
 
-  $('.addition').on('click', function(event){
-    event.preventDefault();
+  $('.clear').on('click').empty();
 
-    $.each($("#calculator").serializeArray(), function(i, field){
-        values[field.name] = field.value;
-    });
-
-    // var value = $('#calculator').serializeArray();
-    // equation.numX = value[0];
-    // equation.numY = value[1];
-    // equation.operand = $('button').className;
-
-    // console.log('This is value: --->', value);
-    console.log('This is input1.value: ', values.input1);
-    console.log('This is input2.value: ', values.input2);
-    console.log('*** This is equation object: ---->>>', values);
-
-    // console.log('numX : ', numX);
-    // console.log('numY :', numY);
-    // console.log('operand :', operand);
-
-    // $('#calculator').find('input[type=text]').val('');
-
-    $.ajax({
-      type: 'POST',
-      url: '/operand/addition',
-      data: values,
-      success: function(data){
-        console.log('Successful ajax POST: ', data);
-        $.ajax({
-          type: 'GET',
-          url: '/addition/answer',
-          success: function(data){
-              $('#answer').text(data);
-            }
-          });
-        }
-      });
-    });
-
-    $('.subtraction').on('click', function(event){
-      event.preventDefault();
-      var value = $('#calculator').serializeArray();
-      console.log('This is value: --->', value);
-      console.log('This is input1.value: ', value[0].value);
-      console.log('This is input2.value: ', value[1].value);
-
-      // $('#calculator').find('input[type=text]').val('');
-
-      $.ajax({
-        type: 'POST',
-        url: '/operand/subtraction',
-        data: value,
-        success: function(data){
-          console.log('Successful ajax POST: ', data);
-          $.ajax({
-            type: 'GET',
-            url: '/subtraction/answer',
-            success: function(data){
-                $('#answer').text(data);
-              }
-            });
-          }
-        });
-      });
-
-      $('.multiplication').on('click', function(event){
-        event.preventDefault();
-        var value = $('#calculator').serializeArray();
-        console.log('This is value: --->', value);
-        console.log('This is input1.value: ', value[0].value);
-        console.log('This is input2.value: ', value[1].value);
-
-        // $('#calculator').find('input[type=text]').val('');
-
-        $.ajax({
-          type: 'POST',
-          url: '/operand/multiplication',
-          data: value,
-          success: function(data){
-            console.log('Successful ajax POST: ', data);
-            $.ajax({
-              type: 'GET',
-              url: '/multiplication/answer',
-              success: function(data){
-                  $('#answer').text(data);
-                }
-              });
-            }
-          });
-        });
-
-        $('.division').on('click', function(event){
-          event.preventDefault();
-          var value = $('#calculator').serializeArray();
-          console.log('This is value: --->', value);
-          console.log('This is input1.value: ', value[0].value);
-          console.log('This is input2.value: ', value[1].value);
-
-          // $('#calculator').find('input[type=text]').val('');
-
-          $.ajax({
-            type: 'POST',
-            url: '/operand/division',
-            data: value,
-            success: function(data){
-              console.log('Successful ajax POST: ', data);
-              $.ajax({
-                type: 'GET',
-                url: '/division/answer',
-                success: function(data){
-                    $('#answer').text(data);
-                  }
-                });
-              }
-            });
-          });
-
+  $('.addition').on('click', calculate);
+  $('.subtraction').on('click', calculate);
+  $('.multiplication').on('click', calculate);
+  $('.division').on('click', calculate);
 
   });
 
 
+var calculate = function(event){
+  event.preventDefault();
+
+  $.each($("#calculator").serializeArray(), function(i, field){
+      values[field.name] = field.value;
+  });
+
+  var objEquation = {
+      'numX' : values.input1,
+      'numY' : values.input2,
+      'operand' : $(this).data().operand
+    }
+  console.log(objEquation);
+
+  // console.log('This is value: --->', value);
+  // console.log('This is input1.value: ', values.input1);
+  // console.log('This is input2.value: ', values.input2);
+  // console.log('*** This is equation object: ---->>>', objEquation);
+
+  // $('#calculator').find('input[type=text]').val('');
+
+  $.ajax({
+    type: 'POST',
+    url: '/operand/' + objEquation.operand + '/',
+    data: objEquation,
+    success: function(data){
+      console.log('Successful ajax POST: ', data);
+      $.ajax({
+        type: 'GET',
+        url: '/answer/',
+        success: function(data){
+            $('#answer').text(data.response);
+            console.log('ANSWER: ',data);
+          }
+        });
+      }
+    });
+    $('#calculator')[0].reset();
+  }
 
 
 
--- server/server.js
+++ server/server.js
@@ -15,25 +15,54 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', (process.env.PORT || 5000));
 
+var Equation = function(numX, numY){
+  this.numX = numX,
+  this.numY = numY
+}
+
 app.post('/operand/:id/', function(req,res){
     // console.log(req.params);
 
     if(req.params.id == 'addition'){
+      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
       console.log('Look, we are Adding!');
-      // console.log(data);
-      // var answer = 99;
+      console.log(data);
+      console.log(data.numX, data.numY);
+      answer = data.numX + data.numY;
+      console.log('SERVER-SIDE Answer: ', answer);
+      res.send({response: answer});
+
     } else if (req.params.id == 'subtraction'){
+      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+      answer = data.numX - data.numY;
       console.log('Look, we are Subtracting!');
+      console.log('SERVER-SIDE Answer: ', answer);
+      res.send({response: answer});
+
     } else if (req.params.id == 'multiplication'){
+      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+      answer = data.numX * data.numY;
       console.log('Look, we are Multiplying!');
+      console.log('SERVER-SIDE Answer: ', answer);
+      res.send({response: answer});
+
     } else if (req.params.id == 'division'){
+      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+      answer = data.numX / data.numY;
       console.log('Look, we are Dividing!');
+      console.log('SERVER-SIDE Answer: ', answer);
+      res.send({response: answer});
     }
 });
 
 // app.get('/addition/answer', function(req, res) {
 //
 // });
+
+app.get('/answer', function (req, res) {
+  res.send({response: answer});
+  console.log('Server "GET": ', answer);
+});
 
 app.get('/*', function(req,res){
     var file = req.params[0] || '/views/index.html';
