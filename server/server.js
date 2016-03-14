5bc1ceb123786425008fabb6212f7934676602b4
Mbob023 <armcgrathmedia@gmail.com>
2016-03-14 13:01:52 -0500

    server update indv ports

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require("path");
 var answer = 0;
-var data = {};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,53 +14,41 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', (process.env.PORT || 5000));
 
-var Equation = function(numX, numY){
-  this.numX = numX,
-  this.numY = numY
-}
 
-app.post('/operand/:id/', function(req,res){
+app.post('/operand/addition/', function(req,res){
+    answer = parseInt(req.body.numX) + parseInt(req.body.numY);
     // console.log(req.params);
-
-    if(req.params.id == 'addition'){
-      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
-      console.log('Look, we are Adding!');
-      console.log(data);
-      console.log(data.numX, data.numY);
-      answer = data.numX + data.numY;
-      console.log('SERVER-SIDE Answer: ', answer);
-      res.send({response: answer});
-
-    } else if (req.params.id == 'subtraction'){
-      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
-      answer = data.numX - data.numY;
-      console.log('Look, we are Subtracting!');
-      console.log('SERVER-SIDE Answer: ', answer);
-      res.send({response: answer});
-
-    } else if (req.params.id == 'multiplication'){
-      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
-      answer = data.numX * data.numY;
-      console.log('Look, we are Multiplying!');
-      console.log('SERVER-SIDE Answer: ', answer);
-      res.send({response: answer});
-
-    } else if (req.params.id == 'division'){
-      var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
-      answer = data.numX / data.numY;
-      console.log('Look, we are Dividing!');
-      console.log('SERVER-SIDE Answer: ', answer);
-      res.send({response: answer});
-    }
+      // var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+    console.log('Look, we are Adding!');
+    console.log('SERVER-SIDE Answer: ', answer);
+    res.send({response: answer});
 });
 
-// app.get('/addition/answer', function(req, res) {
-//
-// });
+app.post('/operand/subtraction/', function(req,res){
+    answer = parseInt(req.body.numX) - parseInt(req.body.numY);
+    // console.log(req.params);
+      // var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+    console.log('Look, we are Adding!');
+    console.log('SERVER-SIDE Answer: ', answer);
+    res.send({response: answer});
+});
 
-app.get('/answer', function (req, res) {
-  res.send({response: answer});
-  console.log('Server "GET": ', answer);
+app.post('/operand/multiplication/', function(req,res){
+    answer = parseInt(req.body.numX) * parseInt(req.body.numY);
+    // console.log(req.params);
+      // var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+    console.log('Look, we are Adding!');
+    console.log('SERVER-SIDE Answer: ', answer);
+    res.send({response: answer});
+});
+
+app.post('/operand/division/', function(req,res){
+    answer = parseInt(req.body.numX) / parseInt(req.body.numY);
+    // console.log(req.params);
+      // var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+    console.log('Look, we are Adding!');
+    console.log('SERVER-SIDE Answer: ', answer);
+    res.send({response: answer});
 });
 
 app.get('/*', function(req,res){
@@ -77,3 +64,62 @@ var server = app.listen((app.get("port")), function(){
   var port = server.address().port;
     console.log('Listening on port:', port);
 });
+
+
+
+
+
+//END _-_-_-_|
+
+
+
+//OLD CODE (I'm a digital pack-rat, actually I'm just a pack-rat!)
+
+
+
+// var data = {};
+// var Equation = function(numX, numY){
+//   this.numX = numX,
+//   this.numY = numY
+//  }
+
+// app.post('/operand/:id/', function(req,res){
+//     // console.log(req.params);
+//
+//     if(req.params.id == 'addition'){
+//       var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+//       console.log('Look, we are Adding!');
+//       console.log(data);
+//       console.log(data.numX, data.numY);
+//       answer = data.numX + data.numY;
+//       console.log('SERVER-SIDE Answer: ', answer);
+//       res.send({response: answer});
+//
+//     } else if (req.params.id == 'subtraction'){
+//       var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+//       answer = data.numX - data.numY;
+//       console.log('Look, we are Subtracting!');
+//       console.log('SERVER-SIDE Answer: ', answer);
+//       res.send({response: answer});
+//
+//     } else if (req.params.id == 'multiplication'){
+//       var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+//       answer = data.numX * data.numY;
+//       console.log('Look, we are Multiplying!');
+//       console.log('SERVER-SIDE Answer: ', answer);
+//       res.send({response: answer});
+//
+//     } else if (req.params.id == 'division'){
+//       var data = new Equation(parseInt(req.body.numX), parseInt(req.body.numY));
+//       answer = data.numX / data.numY;
+//       console.log('Look, we are Dividing!');
+//       console.log('SERVER-SIDE Answer: ', answer);
+//       res.send({response: answer});
+//     }
+// });
+
+
+// app.get('/answer', function (req, res) {
+//   res.send({response: answer});
+//   console.log('Server "GET": ', answer);
+// });
